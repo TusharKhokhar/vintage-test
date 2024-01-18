56a83f6d147ca7ac06116a29e74d9416c814ee07
TusharKhokhar <khokhartushar15@gmail.com>
2024-01-18 16:20:53 +0530

    update script file

--- src/scripts/category.js
+++ src/scripts/category.js
@@ -2,8 +2,19 @@ const { MongoClient, ObjectId } = require("mongodb");
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname + "/./../../.env") });
 
+function convertArrayToObjectId(script) {
+  return script.map((item) => {
+    const id = item._id.$oid;
+
+    return {
+      ...item,
+      _id: new ObjectId(`${id}`),
+    };
+  });
+}
+
 const url = process.env.DATABASE_URI;
-const dbName = "vintage_prod";
+const dbName = "admin";
 
 const client = new MongoClient(url);
 client.connect(async function (err) {
@@ -15,10 +26,10 @@ client.connect(async function (err) {
   console.log("Connected successfully to server");
 
   const db = client.db(dbName);
-  const collection = db.collection("test");
+  const collection = db.collection("categories");
   const categoryScript = [
     {
-      _id: new ObjectId("65893d98d29bf691f1ec85c9"),
+      _id: { $oid: "65893d98d29bf691f1ec85c9" },
       id: "1520566f-4d96-6dcb-7e50-489ad68db5e0",
       name: "Wall ",
       slug: "antique-vintage-lighting-wall-fixtures",
@@ -32,12 +43,10 @@ client.connect(async function (err) {
       keywords:
         "Restored Vintage and Antique Porch Light Fixtures. Old Lighting for Arts & Crafts, Spanish Revival, Bungalow and Craftsman. Restored Chandeliers, Table Lamps, Wall Sconces, and Porch Lights for renovation, restoration",
       sequence: "1",
-      date_created: "2008-08-07 06:05:31",
-      date_modified: "2010-12-09 10:38:58",
-      deleted: false,
+      deleted: "0",
     },
     {
-      _id: new ObjectId("65893d98d29bf691f1ec85ca"),
+      _id: { $oid: "65893d98d29bf691f1ec85ca" },
       id: "e81b85fb-73ff-7045-b27f-489ad77387ea",
       name: "Ceiling ",
       slug: "vintage-ceiling-light-fixtures-antique",
@@ -52,12 +61,10 @@ client.connect(async function (err) {
       keywords:
         "Restored Vintage and Antique Light Fixtures. Old Lighting for Arts & Crafts, Spanish Revival, Bungalow and Craftsman. Restored Chandeliers, Table Lamps, Wall Sconces, and Porch Lights for renovation, restoration",
       sequence: "101",
-      date_created: "2008-08-07 06:08:06",
-      date_modified: "2010-12-09 10:41:02",
-      deleted: false,
+      deleted: "0",
     },
     {
-      _id: new ObjectId("65893d98d29bf691f1ec85cb"),
+      _id: { $oid: "65893d98d29bf691f1ec85cb" },
       id: "9f0f6ef8-95b8-e721-838e-489ad8a6e08e",
       name: "Porch ",
       slug: "restored-antique-porch-lighting-vintage-fixtures",
@@ -72,12 +79,10 @@ client.connect(async function (err) {
       keywords:
         "Restored Vintage and Antique Porch Light Fixtures. Old Lighting for Arts & Crafts, Spanish Revival, Bungalow and Craftsman. Restored Chandeliers, Table Lamps, Wall Sconces, and Porch Lights for renovation, restoration",
       sequence: "201",
-      date_created: "2008-08-07 06:11:28",
-      date_modified: "2010-12-08 08:12:32",
-      deleted: false,
+      deleted: "0",
     },
     {
-      _id: new ObjectId("65893d98d29bf691f1ec85cc"),
+      _id: { $oid: "65893d98d29bf691f1ec85cc" },
       id: "b9f6051f-ed72-4a0c-4d5f-489ad9954012",
       name: "Table ",
       slug: "antique-and-vintage-table-lighting-fixtures",
@@ -91,31 +96,10 @@ client.connect(async function (err) {
       keywords:
         "Restored Vintage and Antique Table Light Lamp Fixtures. Old Lighting for Arts & Crafts, Spanish Revival, Bungalow and Craftsman. Restored Chandeliers, Table Lamps, Wall Sconces, and Porch Lights for renovation, restoration",
       sequence: "301",
-      date_created: "2008-08-07 06:14:55",
-      date_modified: "2010-12-08 07:28:38",
-      deleted: false,
+      deleted: "0",
     },
     {
-      _id: new ObjectId("65893d98d29bf691f1ec85cd"),
-      id: "11",
-      name: "Sets",
-      slug: "sets-vintage-and-antique-lighting-fixtures-and-lights",
-      extn: ".jpg",
-      description:
-        "Matching antique and vintage lighting fixtures for the entire house or just a few rooms.",
-      is_active: true,
-      page_title: "Restored Vintage Lighting | restored antique lighting",
-      meta_description:
-        "Vintage lights, Antique lights, Antique lighting, Vintage lighting, Arts & Crafts, Bungalow, Lamps, American, Architecturial salvage, Restored, Vintage, Antique Light Fixtures. Old Lighting, Arts & Crafts, Spanish Revival, Bungalow, Craftsman. Restored Chandeliers, Table lamp, slip shade, Wall Sconces, Porch Lights, renovation, restoration, remodel",
-      keywords:
-        "Vintage lights, Antique lights, Antique lighting, Vintage lighting, Arts & Crafts, Bungalow, Lamps, American, Architecturial salvage, Restored, Vintage, Antique Light Fixtures. Old Lighting, Arts & Crafts, Spanish Revival, Bungalow, Craftsman. Restored Chandeliers, Table lamp, slip shade, Wall Sconces, Porch Lights, renovation, restoration, remodel",
-      sequence: "701",
-      date_created: "2008-08-23 01:31:18",
-      date_modified: "2010-12-09 10:43:04",
-      deleted: true,
-    },
-    {
-      _id: new ObjectId("65893d98d29bf691f1ec85ce"),
+      _id: { $oid: "65893d98d29bf691f1ec85ce" },
       id: "daa273f6-cae3-2592-eca7-48afaf779648",
       name: "Shades",
       slug: "antique-lighting-shades-for-vinatge-light-fixtures",
@@ -129,12 +113,10 @@ client.connect(async function (err) {
       keywords:
         "Vintage lights, Antique lights, Antique lighting, Vintage lighting, Arts & Crafts, Bungalow, Lamps, American, Architecturial salvage, Restored, Vintage, Antique Light Fixtures. Old Lighting, Arts & Crafts, Spanish Revival, Bungalow, Craftsman. Restored Chandeliers, Table lamp, slip shade, Wall Sconces, Porch Lights, renovation, restoration, remodel",
       sequence: "401",
-      date_created: "2008-08-23 01:32:57",
-      date_modified: "2010-12-10 05:06:32",
-      deleted: false,
+      deleted: "0",
     },
     {
-      _id: new ObjectId("65893d98d29bf691f1ec85cf"),
+      _id: { $oid: "65893d98d29bf691f1ec85cf" },
       id: "2",
       name: "Related Items",
       slug: "related-items",
@@ -148,12 +130,10 @@ client.connect(async function (err) {
       keywords:
         "Vintage lights, Antique lights, Antique lighting, Vintage lighting, Arts & Crafts, Bungalow, Lamps, American, Architecturial salvage, Restored, Vintage, Antique Light Fixtures. Old Lighting, Arts & Crafts, Spanish Revival, Bungalow, Craftsman. Restored Chandeliers, Table lamp, slip shade, Wall Sconces, Porch Lights, renovation, restoration, remodel",
       sequence: "601",
-      date_created: "2008-08-23 01:45:57",
-      date_modified: "2012-05-28 12:09:57",
-      deleted: false,
+      deleted: "0",
     },
     {
-      _id: new ObjectId("65893d98d29bf691f1ec85d0"),
+      _id: { $oid: "65893d98d29bf691f1ec85d0" },
       id: "93462665-6aba-ea83-93ab-48e388972ae4",
       name: "As Found",
       slug: "as-found",
@@ -168,28 +148,10 @@ client.connect(async function (err) {
       keywords:
         "Bathroom lighting, Restored Vintage lighting and Antique Lighting Fixtures.  Lighting for Arts & Crafts, Spanish Revival, Bungalow and Craftsman.  Restored Chandeliers, Table Lamps, Wall Sconces, and Porch Lights. ",
       sequence: "801",
-      date_created: "2008-10-01 07:25:09",
-      date_modified: "2018-03-25 14:10:52",
-      deleted: false,
+      deleted: "0",
     },
     {
-      _id: new ObjectId("65893d98d29bf691f1ec85d1"),
-      id: "91f0cfbf-d17c-41e5-95ed-48cd90d50fcd",
-      name: "sets",
-      slug: "sets",
-      extn: "",
-      description: "",
-      is_active: true,
-      page_title: "",
-      meta_description: "",
-      keywords: "",
-      sequence: "701",
-      date_created: "2008-09-14 22:29:55",
-      date_modified: "2008-09-14 22:30:44",
-      deleted: true,
-    },
-    {
-      _id: new ObjectId("65893d98d29bf691f1ec85d2"),
+      _id: { $oid: "65893d98d29bf691f1ec85d2" },
       id: "a9da072b-729a-2229-a297-497258d99213",
       name: "Slip Shades",
       slug: "slip-shades-vintage-antique-light-fixtures-vintage-wall-sconces",
@@ -203,44 +165,10 @@ client.connect(async function (err) {
       keywords:
         "Slip shades, vintage, antique light fixtures, antique wall sconces, vintage lighting fixtures, vintage wall sconces, art deco, art nouveau, arts crafts,  ",
       sequence: "501",
-      date_created: "2009-01-17 14:15:43",
-      date_modified: "2011-09-22 09:01:48",
-      deleted: false,
+      deleted: "0",
     },
     {
-      _id: new ObjectId("65893d98d29bf691f1ec85d3"),
-      id: "80628d8f-364d-a8d3-46b4-4a2557b518ee",
-      name: "Related Items",
-      slug: "related-items",
-      extn: "",
-      description: "",
-      is_active: true,
-      page_title: "",
-      meta_description: "",
-      keywords: "",
-      sequence: "901",
-      date_created: "2009-06-02 09:45:48",
-      date_modified: "2009-06-02 09:46:17",
-      deleted: true,
-    },
-    {
-      _id: new ObjectId("65893d98d29bf691f1ec85d4"),
-      id: "69c776fb-800f-5457-d413-4d0123706689",
-      name: "sets",
-      slug: "restored-sets-of-antique-lighting",
-      extn: "",
-      description: "",
-      is_active: false,
-      page_title: "",
-      meta_description: "",
-      keywords: "",
-      sequence: "1001",
-      date_created: "2010-12-09 10:44:04",
-      date_modified: "2010-12-09 10:44:04",
-      deleted: true,
-    },
-    {
-      _id: new ObjectId("65893d98d29bf691f1ec85d5"),
+      _id: { $oid: "65893d98d29bf691f1ec85d5" },
       id: "1",
       name: "Sets",
       slug: "sets",
@@ -253,12 +181,10 @@ client.connect(async function (err) {
       keywords:
         "sets restored vintage lighting, sets vintage lighting fixtures",
       sequence: "701",
-      date_created: "2010-12-09 10:45:34",
-      date_modified: "2010-12-15 07:58:10",
-      deleted: false,
+      deleted: "0",
     },
     {
-      _id: new ObjectId("65893d98d29bf691f1ec85d6"),
+      _id: { $oid: "65893d98d29bf691f1ec85d6" },
       id: "2dc17d69-572e-aea1-21b2-52aefc391ce9",
       name: "NEWS",
       slug: "news",
@@ -269,16 +195,18 @@ client.connect(async function (err) {
       meta_description: "",
       keywords: "",
       sequence: "1101",
-      date_created: "2013-12-16 05:14:24",
-      date_modified: "2014-05-28 05:58:19",
-      deleted: false,
+      deleted: "0",
+      updatedAt: "2014-05-28T00:28:19.000Z",
+      createdAt: "2013-12-15T23:44:24.000Z",
     },
   ];
 
+  const Categories = convertArrayToObjectId(categoryScript);
+
   try {
-    const result = await collection.insertMany(categoryScript);
+    const result = await collection.insertMany(Categories);
     console.log(`${result.insertedCount} documents inserted.`);
-    // const deleteResult = await collection.deleteMany({ deleted: true });  
+    // const deleteResult = await collection.deleteMany({ deleted: true });
     // console.log(`${deleteResult.deletedCount} documents deleted.`);
   } catch (error) {
     console.error("Error occurred while inserting documents:", error);
--- src/scripts/year.js
+++ src/scripts/year.js
@@ -17,66 +17,66 @@ client.connect(async function (err) {
 
   const db = client.db(dbName);
   const collection = db.collection("years");
-  const categoryScript = [
+  const yearScript = [
     {
-      _id: new ObjectId("658bc433af7c1bd944177088"),
+      _id: ObjectId("658bc433af7c1bd944177088"),
       id: "16d4e6a5-f180-8744-611e-489adae95f69",
       type: "year",
       name: "1885-1910",
       slug: "1885-1910-antique-lighting-fixtures-vintage-lights",
       sequence: "500",
-      date_created: "2008-08-07 06:19:42",
-      date_modified: "2010-11-21 07:09:27",
-      deleted: false,
+      deleted: "0",
+      createdAt: "2008-08-07 06:19:42",
+      updatedAt: "2010-11-21 07:09:27",
     },
     {
-      _id: new ObjectId("658bc433af7c1bd944177089"),
+      _id: ObjectId("658bc433af7c1bd944177089"),
       id: "a2e3d707-395d-545c-fab1-489ada3c9a0e",
       type: "year",
       name: "1910-1920",
       slug: "1910-1920-antique-lighting-fixture-vintage-lights",
       sequence: "600",
-      date_created: "2008-08-07 06:20:03",
-      date_modified: "2010-11-21 07:10:02",
-      deleted: false,
+      deleted: "0",
+      createdAt: "2008-08-07 06:20:03",
+      updatedAt: "2010-11-21 07:10:02",
     },
     {
-      _id: new ObjectId("658bc433af7c1bd94417708a"),
+      _id: ObjectId("658bc433af7c1bd94417708a"),
       id: "ea2c0392-a6e7-2a88-b769-489adaf9da99",
       type: "year",
       name: "1920-1930",
       slug: "1920-1930-vintage-lighitng-fixtures-antique-lights",
       sequence: "700",
-      date_created: "2008-08-07 06:20:19",
-      date_modified: "2010-11-21 07:10:23",
-      deleted: false,
+      deleted: "0",
+      createdAt: "2008-08-07 06:20:19",
+      updatedAt: "2010-11-21 07:10:23",
     },
     {
-      _id: new ObjectId("658bc433af7c1bd94417708b"),
+      _id: ObjectId("658bc433af7c1bd94417708b"),
       id: "627a43a7-338b-e06c-e37f-489ada7f4953",
       type: "year",
       name: "1930-1940",
       slug: "1930-1940",
       sequence: "800",
-      date_created: "2008-08-07 06:20:36",
-      date_modified: "2008-08-07 06:20:36",
-      deleted: false,
+      deleted: "0",
+      createdAt: "2008-08-07 06:20:36",
+      updatedAt: "2008-08-07 06:20:36",
     },
     {
-      _id: new ObjectId("658bc433af7c1bd94417708c"),
+      _id: ObjectId("658bc433af7c1bd94417708c"),
       id: "1e0b7b77-418e-892c-3af7-489ada3a5311",
       type: "year",
       name: "1940+",
       slug: "1940-vintage-lighting-fixtures-antique-lights",
       sequence: "900",
-      date_created: "2008-08-07 06:21:01",
-      date_modified: "2010-11-21 07:10:43",
-      deleted: false,
+      deleted: "0",
+      createdAt: "2008-08-07 06:21:01",
+      updatedAt: "2010-11-21 07:10:43",
     },
   ];
 
   try {
-    const result = await collection.insertMany(categoryScript);
+    const result = await collection.insertMany(yearScript);
     console.log(`${result.insertedCount} documents inserted.`);
   } catch (error) {
     console.error("Error occurred while inserting documents:", error);
